8f3241d78ef51e260175c3d0ceb6befa3e30bf56
jynng <bieeelg2@gmail.com>
2020-03-02 20:18:22 -0300

    Updates

---
+++ precache-manifest.327384fc2b1f679461ca730f49a936b4.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88ec460da42da3a0479b1fda4480394a",
+    "url": "/quizz/index.html"
+  },
+  {
+    "revision": "3d5cf1a90dc816e82f13",
+    "url": "/quizz/static/css/main.47c5bf48.chunk.css"
+  },
+  {
+    "revision": "47cd49b95db8656f34c7",
+    "url": "/quizz/static/js/2.ee006b79.chunk.js"
+  },
+  {
+    "revision": "65f61a1b574cbfdae1aa1e0f7a13f7ca",
+    "url": "/quizz/static/js/2.ee006b79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d5cf1a90dc816e82f13",
+    "url": "/quizz/static/js/main.7a1910ff.chunk.js"
+  },
+  {
+    "revision": "67f854a7c5336e73ba38",
+    "url": "/quizz/static/js/runtime-main.cb8cba71.js"
+  },
+  {
+    "revision": "94373d00def0180eec4a95e5c7d7c31e",
+    "url": "/quizz/static/media/HVD Fonts - MikadoBlack.94373d00.otf"
+  },
+  {
+    "revision": "354fdb77676079a083acf3ad4e901013",
+    "url": "/quizz/static/media/HVD Fonts - MikadoBold.354fdb77.otf"
+  },
+  {
+    "revision": "d5bb463cf41e585433eed991e382bfe3",
+    "url": "/quizz/static/media/HVD Fonts - MikadoLight.d5bb463c.otf"
+  },
+  {
+    "revision": "d870bb606ccf40d775f14e60327fe91c",
+    "url": "/quizz/static/media/HVD Fonts - MikadoMedium.d870bb60.otf"
+  },
+  {
+    "revision": "cc40646f2a75f347de736d47ac3cdb44",
+    "url": "/quizz/static/media/HVD Fonts - MikadoRegular.cc40646f.otf"
+  },
+  {
+    "revision": "4afb24ba48a331f8076a7c0c51f43c47",
+    "url": "/quizz/static/media/HVD Fonts - MikadoUltra.4afb24ba.otf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/quizz/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/quizz/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/quizz/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/quizz/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/quizz/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "a974fa9e13cbc686a0437de5a375dfbf",
+    "url": "/quizz/static/media/general.a974fa9e.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quizz/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/quizz/static/media/themify.2c454669.eot"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/quizz/static/media/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "cba68f986e60df8c74f4a53c3e39595c",
+    "url": "/quizz/static/media/themify.cba68f98.svg"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/quizz/static/media/themify.e23a7dca.ttf"
+  }
+]);
